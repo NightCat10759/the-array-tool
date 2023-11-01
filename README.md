b95b8bc40c24ee974c11d8932f7a908e858191bf
NightCat10759 <upioupioupio@yahoo.com.tw>
2023-11-01 20:49:25 +0800

    add: insertSingleQuote function

--- README.md
+++ README.md
@@ -1,18 +1,16 @@
-* the-array-tool
+- the-array-tool
 
+* Sample Input
+  arr: [ 1, 2, 3 ]
+  arr2: [ 1, 2, 4 ]
 
-- Sample Input
-arr:  [ 1, 2, 3 ]
-arr2:  [ 1, 2, 4 ]
+* Function
 
+\*\* removeAll
+removeAll(arr,arr2): [ 3 ]
 
-- Function
+\*\* retainAll
+retainAll(arr,arr2): [ 1, 2 ]
 
-** removeAll
-removeAll(arr,arr2):  [ 3 ]
-
-** retainAll
-retainAll(arr,arr2):  [ 1, 2 ]
-
-** rightOutterJoin
-rightOutterJoin(arr,arr2):  [ 4 ]
+\*\* rightOutterJoin
+rightOutterJoin(arr,arr2): [ 4 ]
--- index.js
+++ index.js
@@ -1,45 +1,56 @@
-
 class TheArrayTool {
-
-    static removeAll(left, right) {
-        try{
-            let res = []
-            for (let data of left) {
-                if(!right.includes(data)) res.push(data)
-            }
-            return res
-        } catch(e) {
-            console.error(e)
-            throw e
-        }
+  static removeAll(left, right) {
+    try {
+      let res = [];
+      for (let data of left) {
+        if (!right.includes(data)) res.push(data);
+      }
+      return res;
+    } catch (e) {
+      console.error(e);
+      throw e;
     }
+  }
 
-    static retainAll(left, right) {
-        try{
-            let res = []
-            for (let data of left) {
-                if(right.includes(data)) res.push(data)
-            }
-            return res
-        } catch(e) {
-            console.error(e)
-            throw e
-        }
+  static retainAll(left, right) {
+    try {
+      let res = [];
+      for (let data of left) {
+        if (right.includes(data)) res.push(data);
+      }
+      return res;
+    } catch (e) {
+      console.error(e);
+      throw e;
     }
+  }
 
-    static rightOutterJoin(left, right) {
-        try{
-            let res = []
-            for (let data of right) {
-                if(!left.includes(data)) res.push(data)
-            }
-            return res
-        } catch(e) {
-            console.error(e)
-            throw e
-        }
+  static rightOutterJoin(left, right) {
+    try {
+      let res = [];
+      for (let data of right) {
+        if (!left.includes(data)) res.push(data);
+      }
+      return res;
+    } catch (e) {
+      console.error(e);
+      throw e;
     }
+  }
+
+  static insertStr(source, startIndex, newStr) {
+    return source.slice(0, startIndex) + newStr + source.slice(startIndex);
+  }
+
+  static insertSingleQuote(str) {
+    if (str == "" || str == undefined || str == null || typeof str != typeof "")
+      return null;
+    if (str.includes("'")) {
+      const index = str.indexOf("'");
+      return this.insertStr(str, index, "'");
+    }
+    return str;
+  }
 }
 
-
-module.exports = TheArrayTool
+module.exports = TheArrayTool;
